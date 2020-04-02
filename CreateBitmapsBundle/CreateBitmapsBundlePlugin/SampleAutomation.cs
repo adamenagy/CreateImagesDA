d3ca73c665d1612f5ab9d4e4fc5eb0d7717b3bfb
Adam Nagy <adam_e_nagy@hotmail.com>
2020-04-02 20:46:14 +0100

    First commit

---
+++ CreateBitmapsBundle/CreateBitmapsBundlePlugin/SampleAutomation.cs
@@ -0,0 +1,107 @@
+﻿/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+using Inventor;
+using Autodesk.Forge.DesignAutomation.Inventor.Utils;
+
+namespace CreateBitmapsBundlePlugin
+{
+    [ComVisible(true)]
+    public class SampleAutomation
+    {
+        private readonly InventorServer inventorApplication;
+
+        public SampleAutomation(InventorServer inventorApp)
+        {
+            inventorApplication = inventorApp;
+        }
+
+        public void Run(Document doc)
+        {
+            LogTrace("Run called with {0}", doc.DisplayName);
+
+            RunWithArguments(doc, null);
+        }
+
+        public void RunWithArguments(Document doc, NameValueMap map)
+        {
+            LogTrace("Processing " + doc.FullFileName);
+
+            dynamic invDoc = doc;
+            ViewOrientationTypeEnum[] orientations = {
+                ViewOrientationTypeEnum.kIsoTopRightViewOrientation,
+                ViewOrientationTypeEnum.kIsoTopLeftViewOrientation,
+                ViewOrientationTypeEnum.kBackViewOrientation
+            };
+            var imagesFolder = System.IO.Path.Combine(
+                System.IO.Directory.GetCurrentDirectory(), "OutputImages");
+            foreach (var orientation in orientations)
+            {
+                var fileName = orientation.ToString() + ".png";
+                var filePath = System.IO.Path.Combine(imagesFolder, fileName);
+                Camera cam = inventorApplication.TransientObjects.CreateCamera();
+                cam.SceneObject = invDoc.ComponentDefinition;
+                cam.ViewOrientationType = orientation;
+                cam.Fit();
+                cam.ApplyWithoutTransition();
+                cam.SaveAsBitmap(filePath, 200, 200, Type.Missing, Type.Missing);
+                LogTrace($"Saved image as {filePath}");
+            }
+        }
+
+        #region Logging utilities
+
+        /// <summary>
+        /// Log message with 'trace' log level.
+        /// </summary>
+        private static void LogTrace(string format, params object[] args)
+        {
+            Trace.TraceInformation(format, args);
+        }
+
+        /// <summary>
+        /// Log message with 'trace' log level.
+        /// </summary>
+        private static void LogTrace(string message)
+        {
+            Trace.TraceInformation(message);
+        }
+
+        /// <summary>
+        /// Log message with 'error' log level.
+        /// </summary>
+        private static void LogError(string format, params object[] args)
+        {
+            Trace.TraceError(format, args);
+        }
+
+        /// <summary>
+        /// Log message with 'error' log level.
+        /// </summary>
+        private static void LogError(string message)
+        {
+            Trace.TraceError(message);
+        }
+
+        #endregion
+    }
+}
---
+++ CreateBitmapsBundle/Interaction/Publisher.Custom.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using Autodesk.Forge.DesignAutomation.Model;
+
+namespace Interaction
+{
+    /// <summary>
+    /// Customizable part of Publisher class.
+    /// </summary>
+    internal partial class Publisher
+    {
+        /// <summary>
+        /// Constants.
+        /// </summary>
+        private static class Constants
+        {
+            private const int EngineVersion = 23;
+            public static readonly string Engine = $"Autodesk.Inventor+{EngineVersion}";
+
+            public const string Description = "Generates images for the Inventor model";
+
+            internal static class Bundle
+            {
+                public static readonly string Id = "CreateBitmapsBundle";
+                public const string Label = "alpha";
+
+                public static readonly AppBundle Definition = new AppBundle
+                {
+                    Engine = Engine,
+                    Id = Id,
+                    Description = Description
+                };
+            }
+
+            internal static class Activity
+            {
+                public static readonly string Id = Bundle.Id;
+                public const string Label = Bundle.Label;
+            }
+
+            internal static class Parameters
+            {
+                public const string InventorDoc = nameof(InventorDoc);
+                public const string OutputImages = nameof(OutputImages);
+            }
+        }
+
+
+        /// <summary>
+        /// Get command line for activity.
+        /// </summary>
+        private static List<string> GetActivityCommandLine()
+        {
+            return new List<string> { $"$(engine.path)\\InventorCoreConsole.exe /al $(appbundles[{Constants.Activity.Id}].path) /i $(args[{Constants.Parameters.InventorDoc}].path)" };
+        }
+
+        /// <summary>
+        /// Get activity parameters.
+        /// </summary>
+        private static Dictionary<string, Parameter> GetActivityParams()
+        {
+            return new Dictionary<string, Parameter>
+                    {
+                        {
+                            Constants.Parameters.InventorDoc,
+                            new Parameter
+                            {
+                                Verb = Verb.Get,
+                                Description = "IPT file to process"
+                            }
+                        },
+                        {
+                            Constants.Parameters.OutputImages,
+                            new Parameter
+                            {
+                                Verb = Verb.Put,
+                                Zip = true,
+                                LocalName = "OutputImages",
+                                Description = "Resulting images",
+                                Ondemand = false,
+                                Required = true
+                            }
+                        }
+                    };
+        }
+
+        /// <summary>
+        /// Get arguments for workitem.
+        /// </summary>
+        private static Dictionary<string, IArgument> GetWorkItemArgs()
+        {
+            // TODO: update the URLs below with real values
+            return new Dictionary<string, IArgument>
+                    {
+                        {
+                            Constants.Parameters.InventorDoc,
+                            new XrefTreeArgument
+                            {
+                                Url = "https://developer.api.autodesk.com/oss/v2/signedresources/446b13c0-f5ae-4c2e-8212-fff87ec5b19e?region=US"
+                            }
+                        },
+                        {
+                            Constants.Parameters.OutputImages,
+                            new XrefTreeArgument
+                            {
+                                Verb = Verb.Put,
+                                Url = "https://developer.api.autodesk.com/oss/v2/signedresources/c220262e-0a2b-42a0-bb98-9ab4cb2a640b?region=US"
+                            }
+                        }
+                    };
+        }
+    }
+}
